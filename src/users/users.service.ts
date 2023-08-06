babe54732e55867869b9950b00e2c963fb3d898e
LeandroFilie <leandro.gfilie@gmail.com>
2023-08-06 16:55:11 -0300

    feat: added password encrypt

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,22 +1,26 @@
 import { ConflictException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { PrismaService } from 'src/database/prisma.service';
+import { hash } from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly prismaService: PrismaService) {}
 
   async create(createUserDto: CreateUserDto) {
+    const { name, email, password } = createUserDto;
     const emailAlreadyExists = await this.prismaService.user.findUnique({
-      where: { email: createUserDto.email },
+      where: { email },
     });
 
     if (emailAlreadyExists) {
       throw new ConflictException('This email is already in use');
     }
 
+    const hashedPassword = await hash(password, 12);
+
     const user = await this.prismaService.user.create({
-      data: createUserDto,
+      data: { name, email, password: hashedPassword },
     });
 
     return user;
