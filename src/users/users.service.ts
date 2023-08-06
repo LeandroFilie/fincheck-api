3e1beb31f2d1310f90f575121130d68e1dc7d7ef
LeandroFilie <leandro.gfilie@gmail.com>
2023-08-06 16:44:59 -0300

    feat: added verify email already exists

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { PrismaService } from 'src/database/prisma.service';
 
@@ -7,6 +7,14 @@ export class UsersService {
   constructor(private readonly prismaService: PrismaService) {}
 
   async create(createUserDto: CreateUserDto) {
+    const emailAlreadyExists = await this.prismaService.user.findUnique({
+      where: { email: createUserDto.email },
+    });
+
+    if (emailAlreadyExists) {
+      throw new ConflictException('This email is already in use');
+    }
+
     const user = await this.prismaService.user.create({
       data: createUserDto,
     });
