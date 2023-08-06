2f11b263285d26c69262157c88a6fd036a7db51d
LeandroFilie <leandro.gfilie@gmail.com>
2023-08-06 20:38:52 -0300

    feat: added route to return user infos

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -4,4 +4,11 @@ import { UsersRepository } from 'src/shared/database/repositories/user.repositor
 @Injectable()
 export class UsersService {
   constructor(private readonly usersRepository: UsersRepository) {}
+
+  getUserById(userId: string) {
+    return this.usersRepository.findUnique({
+      select: { name: true, email: true },
+      where: { id: userId },
+    });
+  }
 }
