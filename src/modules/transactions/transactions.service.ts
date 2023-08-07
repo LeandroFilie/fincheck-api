e498bc640ad56a0ee369eb1c9f86bc34922ff848
LeandroFilie <leandro.gfilie@gmail.com>
2023-08-07 20:47:50 -0300

    feat: added verification ownership of bankAccount and category

--- src/modules/transactions/transactions.service.ts
+++ src/modules/transactions/transactions.service.ts
@@ -3,16 +3,20 @@ import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { UpdateTransactionDto } from './dto/update-transaction.dto';
 import { TransactionsRepository } from 'src/shared/database/repositories/transactions.repositories';
 import { ValidateBankAccountOwnershipService } from '../bank-accounts/services/validate-bank-account-ownership.service';
+import { ValidateCategoryOwnershipService } from '../categories/services/validate-category-ownership.service';
 
 @Injectable()
 export class TransactionsService {
   constructor(
     private readonly transactionsRepository: TransactionsRepository,
     private readonly validateBankAccountOwnershipService: ValidateBankAccountOwnershipService,
+    private readonly validateCategoryOwnershipService: ValidateCategoryOwnershipService,
   ) {}
 
-  create(createTransactionDto: CreateTransactionDto) {
-    return 'This action adds a new transaction';
+  async create(userId: string, createTransactionDto: CreateTransactionDto) {
+    const { bankAccountId, categoryId } = createTransactionDto;
+
+    await this.validateEntitiesOwnerShip({ userId, bankAccountId, categoryId });
   }
 
   findAllByUserId(userId: string) {
@@ -27,5 +31,23 @@ export class TransactionsService {
 
   remove(id: number) {
     return `This action removes a #${id} transaction`;
+  }
+
+  private async validateEntitiesOwnerShip({
+    userId,
+    bankAccountId,
+    categoryId,
+  }: {
+    userId: string;
+    bankAccountId: string;
+    categoryId: string;
+  }) {
+    await Promise.all([
+      await this.validateBankAccountOwnershipService.validate(
+        userId,
+        bankAccountId,
+      ),
+      await this.validateCategoryOwnershipService.validate(userId, categoryId),
+    ]);
   }
 }
