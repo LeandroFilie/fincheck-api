fa207f99245e35febd333952b56a3362ee80c5c6
LeandroFilie <leandro.gfilie@gmail.com>
2023-08-07 20:36:07 -0300

    feat: create list transactions

---
+++ src/modules/transactions/transactions.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { CreateTransactionDto } from './dto/create-transaction.dto';
+import { UpdateTransactionDto } from './dto/update-transaction.dto';
+import { TransactionsRepository } from 'src/shared/database/repositories/transactions.repositories';
+import { ValidateBankAccountOwnershipService } from '../bank-accounts/services/validate-bank-account-ownership.service';
+
+@Injectable()
+export class TransactionsService {
+  constructor(
+    private readonly transactionsRepository: TransactionsRepository,
+    private readonly validateBankAccountOwnershipService: ValidateBankAccountOwnershipService,
+  ) {}
+
+  create(createTransactionDto: CreateTransactionDto) {
+    return 'This action adds a new transaction';
+  }
+
+  findAllByUserId(userId: string) {
+    return this.transactionsRepository.findMany({
+      where: { userId },
+    });
+  }
+
+  update(id: number, updateTransactionDto: UpdateTransactionDto) {
+    return `This action updates a #${id} transaction`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} transaction`;
+  }
+}
