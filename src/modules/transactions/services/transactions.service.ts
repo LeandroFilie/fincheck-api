55397b68fd73a40d150653d1657682b6609b3307
LeandroFilie <leandro.gfilie@gmail.com>
2023-08-07 21:16:26 -0300

    feat: added update transaction and validator for ownership of transaction

---
+++ src/modules/transactions/services/transactions.service.ts
@@ -0,0 +1,89 @@
+import { Injectable } from '@nestjs/common';
+import { CreateTransactionDto } from '../dto/create-transaction.dto';
+import { UpdateTransactionDto } from '../dto/update-transaction.dto';
+import { TransactionsRepository } from 'src/shared/database/repositories/transactions.repositories';
+import { ValidateBankAccountOwnershipService } from '../../bank-accounts/services/validate-bank-account-ownership.service';
+import { ValidateCategoryOwnershipService } from '../../categories/services/validate-category-ownership.service';
+import { ValidateTransactionOwnershipService } from './validate-transaction-ownership.service';
+
+@Injectable()
+export class TransactionsService {
+  constructor(
+    private readonly transactionsRepository: TransactionsRepository,
+    private readonly validateBankAccountOwnershipService: ValidateBankAccountOwnershipService,
+    private readonly validateCategoryOwnershipService: ValidateCategoryOwnershipService,
+    private readonly validateTransactionOwnershipService: ValidateTransactionOwnershipService,
+  ) {}
+
+  async create(userId: string, createTransactionDto: CreateTransactionDto) {
+    const { bankAccountId, categoryId, date, name, type, value } =
+      createTransactionDto;
+
+    await this.validateEntitiesOwnerShip({ userId, bankAccountId, categoryId });
+
+    return this.transactionsRepository.create({
+      data: {
+        userId,
+        bankAccountId,
+        categoryId,
+        date,
+        name,
+        type,
+        value,
+      },
+    });
+  }
+
+  findAllByUserId(userId: string) {
+    return this.transactionsRepository.findMany({
+      where: { userId },
+    });
+  }
+
+  async update(
+    userId: string,
+    transactionId: string,
+    updateTransactionDto: UpdateTransactionDto,
+  ) {
+    const { bankAccountId, categoryId, date, name, type, value } =
+      updateTransactionDto;
+
+    await this.validateEntitiesOwnerShip({
+      userId,
+      bankAccountId,
+      categoryId,
+      transactionId,
+    });
+
+    return this.transactionsRepository.update({
+      where: { id: transactionId },
+      data: { bankAccountId, categoryId, date, name, type, value },
+    });
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} transaction`;
+  }
+
+  private async validateEntitiesOwnerShip({
+    userId,
+    bankAccountId,
+    categoryId,
+    transactionId,
+  }: {
+    userId: string;
+    bankAccountId: string;
+    categoryId: string;
+    transactionId?: string;
+  }) {
+    await Promise.all([
+      transactionId &&
+        this.validateTransactionOwnershipService.validate(
+          userId,
+          transactionId,
+        ),
+      this.validateBankAccountOwnershipService.validate(userId, bankAccountId),
+      this.validateCategoryOwnershipService.validate(userId, categoryId),
+    ]);
+  }
+}
